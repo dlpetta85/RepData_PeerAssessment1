684d3cf0af3a0ea43110af08323ef008fab33ed5
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-08 18:18:55 -0300

    sts9

--- rep1.R
+++ rep1.R
@@ -1,3 +1,6 @@
+library("dplyr")
+library("lattice")
+
 ##Getting the source
 ##1. Code for reading in the dataset and/or processing the data
 if (!file.exists("activity.csv") )
@@ -13,72 +16,60 @@ data <- read.csv("activity.csv")
 ##2. Histogram of the total number of steps taken each day
 ##3. Mean and median number of steps taken each day
 png('steps.png')
-mov <- aggregate(steps ~ date, data, sum)
-hist(mov$steps, main = paste("Total of steps per Day"), col='Red',xlab='Number of Steps')
+
+mov <- data %>% group_by(date) %>% summarise(steps = sum(steps))
+hist(mov$steps, col="light blue",xlab = "Total number of steps by day", main = "Number of steps by day")
+mean(mov$steps,na.rm = TRUE)
+
 dev.off()
-
-media <- mean(mov$steps)
-media
-mediano <- median(mov$steps)
-mediano
-
 
 ##Working and plotting data of average activity 
 ## 4. Time series plot of the average number of steps taken
 ## 5. The 5-minute interval that, on average, contains the maximum number of steps
 png('interval.png')
-espaço <- aggregate(steps ~ interval, data, mean)
-plot(espaço$interval,espaço$steps, type="l", xlab="Interval", ylab="Number of Steps",main="Average Number per Day by Interval")
-dev.off()
 
-espaçomax <- espaço[which.max(espaço$steps),1]
-espaçomax
+median(mov$steps,na.rm = TRUE)
+espaço <- data %>% group_by(interval) %>% summarise(steps = mean(steps,na.rm=TRUE))
+plot(espaço$interval,espaço$steps,type="l",xlab="5 minutes interval", ylab="Average nº steps",main="Average daily pattern")
+espaço$interval[which.max(espaço$steps)]
+
+dev.off()
 
 ##Working and plotting data of missing values
 ##6. Code to describe and show a strategy for imputing missing data
 ##7. Histogram of the total number of steps taken each day after missing values are imputed
-
-Mistotal <- sum(!complete.cases(data))
-Mistotal
-
-mediapassos <- aggregate(steps ~ interval, data = data, FUN = mean)
-pegaNA <- numeric()
-for (i in 1:nrow(data)) {
-  obs <- data[i, ]
-  if (is.na(obs$steps)) {
-    steps <- subset(mediapassos, interval == obs$interval)$steps
-  } else {
-    steps <- obs$steps
-  }
-  pegaNA <- c(pegaNA, steps)
-}
-
-atividades <- data
-atividades$steps <- pegaNA
-
 png('totalpassos.png')
-agregado <- aggregate(steps ~ date, data = atividades, sum, na.rm = TRUE)
-hist(agregado$steps, main = paste("Total Steps per days"), col="green", xlab="Number of Steps")
+sum(is.na(data$steps))
+
+datatrab <- data
+names(espaço)[2] <- "avg_steps"
+datamesc <- merge(datatrab,espaço)
+datamesc[which(is.na(datamesc$steps)),]$steps <- datamesc[which(is.na(datamesc$steps)),]$avg_steps
+datafim <- datamesc[1:3]
+mov2 <- datafim %>% group_by(date) %>% summarise(steps = sum(steps))
+
+hist(mov2$steps, col="light blue",xlab = "Number of steps", 
+     main = "Total steps per day")
+
 dev.off()
 
-mediatot <- mean(agregado$steps)
-mediatot
-
-medianotot <- median(agregado$steps)
-medianotot
+mean(mov2$steps,na.rm = TRUE)
+median(mov2$steps,na.rm = TRUE)
 
 
 ##Working and plotting data to split pattern
 ##8. Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
 ##9. All of the R code needed to reproduce the results (numbers, plots, etc.) in the report
 png('Comparison.png')
-weekdays  <- c('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday')
 
-atividades$dow = as.factor(ifelse(is.element(weekdays(as.Date(atividades$date)),weekdays), 'weekdays', 'Weekends'))
+datafim <- datafim %>% mutate(day_type = ifelse(grepl("S(at|un)",weekdays(as.Date(datafim$date, 
+                                                                                        format = "%Y-%m-%d"))),"weekend","weekday"))
+datafim$day_type <- as.factor(datafim$day_type)
 
-agregado <- aggregate(steps ~ interval + dow, atividades, mean)
-library(lattice)
-xyplot(agregado$steps ~ agregado$interval|agregado$dow, main="Average Steps - Interval based on days",xlab="Interval", ylab="Steps",layout=c(1,2), type="l")
+espaço2 <- datafim %>% group_by(interval,day_type) %>% summarise(steps = mean(steps,na.rm=TRUE))
+xyplot(steps ~ interval | day_type, data = espaço2, type="l",xlab="Interval",ylab="Number of steps",main="Comparison between weekday and weekend activities",
+       col.line = "red",
+       layout=c(1,2))
 
 dev.off()
 
