f2f53a381dbb2efaf4019f372a8371061e8013cb
dlpetta85 <de.leon.petta@hotmail.com>
2020-04-08 15:40:46 -0300

    sts3

--- rep1.R
+++ rep1.R
@@ -1,4 +1,5 @@
 ##Getting the source
+##1. Code for reading in the dataset and/or processing the data
 if (!file.exists("activity.csv") )
 {
   dlurl <- 'https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip'  
@@ -7,11 +8,28 @@ if (!file.exists("activity.csv") )
 }
 data <- read.csv("activity.csv")  
 
-##Working and plotting data
+
+##Working and plotting data of steps per day
+##2. Histogram of the total number of steps taken each day
+##3. Mean and median number of steps taken each day
 png('steps.png')
 mov <- aggregate(steps ~ date, data, sum)
 hist(mov$steps, main = paste("Total of steps per Day"), col='Red',xlab='Number of Steps')
 dev.off()
 
-med <- mean(mov$steps)
-med
+media <- mean(mov$steps)
+media
+mediano <- median(mov$steps)
+mediano
+
+
+##Working and plotting data of average activity 
+## 4. Time series plot of the average number of steps taken
+png('interval.png')
+espaço <- aggregate(steps ~ interval, data, mean)
+plot(espaço$interval,espaço$steps, type="l", xlab="Interval", ylab="Number of Steps",main="Average Number per Day by Interval")
+dev.off()
+
+espaçomax <- espaço[which.max(espaço$steps),1]
+espaçomax
+
